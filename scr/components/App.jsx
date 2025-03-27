592a7c6f104197afdfceb28dcbe2b247bb9f124b
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-27 08:30:04 +0300

    Create App.jsx

---
+++ scr/components/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-2xl">GitHub User Search</h1>
+      </header>
+      <main className="container mx-auto p-4">
+        {/* Components will go here */}
+      </main>
+    </div>
+  )
+}
+
+export default App
